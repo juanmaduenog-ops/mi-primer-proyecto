43804108282ae804e1a1623eabd6bfc1cfa4489a
Juan Luis Madueño <juan.maduenog@unmsm.edu.pe>
2026-06-09 02:37:18 -0500

    Agregar nuevo saludo

--- hello.py
+++ hello.py
@@ -4,5 +4,8 @@ def saludar(nombre):
 def despedida(nombre):
     return f"Adiós, {nombre}"
 
+def saludo_especial(nombre):
+    return f"Bienvenido, {nombre}. Somos la mejor facultad de la UNMSM."
+
 if __name__ == "__main__":
     print(saludar("UNMSM"))
